b71c3a3173738e09603c4139af9222559341be73
Gesare <gesareayienda@gmail.com>
2020-06-28 22:17:33 +0300

    Added procfile

--- portpro/urls.py
+++ portpro/urls.py
@@ -1,7 +1,7 @@
-"""portpro URL Configuration
+"""awwardproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url,include
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views
+from registration.backends.simple.views import RegistrationView
+from awwardapp.forms import RegisterForm
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('awwardapp.urls')),
+    url(r'^accounts/register/$',
+        RegistrationView.as_view(
+            form_class=RegisterForm
+        ),
+        name='registration_register',
+    ),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
+    url(r'^tinymce/', include('tinymce.urls')),
 ]
