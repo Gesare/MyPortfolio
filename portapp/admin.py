23101e2d6d19b94c18115adeaf79b85420ad20bf
Gesare <gesareayienda@gmail.com>
2020-06-29 15:37:50 +0300

    Deleting files

--- portapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Profile,Projects
-
-
-admin.site.register(Projects)
-admin.site.register(Profile)
--- portapp/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class PortappConfig(AppConfig):
-    name = 'portapp'
-
-    def ready(self):
-        import portapp.signals
